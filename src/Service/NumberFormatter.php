e3d42ed85125941c662cb3f61f15c35631798f37
Olga Zyk <olga.zyk.sln@gmail.com>
2018-05-29 23:08:32 +0300

    Refactoring according PSR-2 standard: change variable name, remove empty lines;

--- src/Service/NumberFormatter.php
+++ src/Service/NumberFormatter.php
@@ -1,36 +1,32 @@
 <?php
 
   namespace App\Service;
-
-
+  
   /**
    * Class NumberFormatter
    * @package App\Service
    */
   class NumberFormatter
   {
-
     /**
      * @param float $number
      * @return string
      */
     public function formatNumber(float $number): string
     {
-
-      $numberFormat = '';
+      $formattedNumber = '';
 
       if (999500 <= $number) {
-        $numberFormat = sprintf("%4.2f", number_format((round(abs($number), -4)), 0, '.', '.')) . 'M';
+        $formattedNumber = sprintf("%4.2f", number_format((round(abs($number), -4)), 0, '.', '.')) . 'M';
       } else if (99950 <= $number && $number < 999500) {
-        $numberFormat = sprintf("%3d", number_format((round(abs($number), -3)), 0, '.', '.')) . 'K';
+        $formattedNumber = sprintf("%3d", number_format((round(abs($number), -3)), 0, '.', '.')) . 'K';
       } else if ($number >= 1000 && $number < 99950) {
-        $numberFormat = str_replace(".", " ", number_format((round(abs($number), 0)), 0, '.', '.'));
+        $formattedNumber = str_replace(".", " ", number_format((round(abs($number), 0)), 0, '.', '.'));
       } else if ($number < 1000) {
-        $numberFormat = number_format(strval(abs($number)), 2, '.', '.');
+        $formattedNumber = number_format(strval(abs($number)), 2, '.', '.');
       }
 
-      return $numberFormat;
-
+      return $formattedNumber;
     }
 
   }
