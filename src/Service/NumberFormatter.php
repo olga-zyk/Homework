18d47486d4800520e6d3b58594b0b1584d75c066
Olga Zyk <olga.zyk.sln@gmail.com>
2018-06-02 22:04:39 +0300

    Add more test cases; Fix code for number formatting;

--- src/Service/NumberFormatter.php
+++ src/Service/NumberFormatter.php
@@ -1,7 +1,7 @@
 <?php
 
   namespace App\Service;
-  
+
   /**
    * Class NumberFormatter
    * @package App\Service
@@ -14,16 +14,17 @@
      */
     public function formatNumber(float $number): string
     {
+      $absoluteNumber = abs($number);
       $formattedNumber = '';
 
-      if (999500 <= $number) {
-        $formattedNumber = sprintf("%4.2f", number_format((round(abs($number), -4)), 0, '.', '.')) . 'M';
-      } else if (99950 <= $number && $number < 999500) {
-        $formattedNumber = sprintf("%3d", number_format((round(abs($number), -3)), 0, '.', '.')) . 'K';
-      } else if ($number >= 1000 && $number < 99950) {
-        $formattedNumber = str_replace(".", " ", number_format((round(abs($number), 0)), 0, '.', '.'));
-      } else if ($number < 1000) {
-        $formattedNumber = number_format(strval(abs($number)), 2, '.', '.');
+      if (999500 <= $absoluteNumber) {
+        $formattedNumber = sprintf("%.2fM", (round($number, -4) / 1000000));
+      } else if (99950 <= $absoluteNumber && $absoluteNumber < 999500) {
+        $formattedNumber = sprintf("%3dK", (round($number, -3)) / 1000);
+      } else if ($absoluteNumber == 999.9999 || ($absoluteNumber >= 1000 && $absoluteNumber < 99950)) {
+        $formattedNumber = number_format((round($number, 0)), 0, '', ' ');
+      } else if ($absoluteNumber < 1000) {
+        $formattedNumber = number_format(strval($number), 2, '.', '.');
       }
 
       return $formattedNumber;
--- tests/Service/NumberFormatterTest.php
+++ tests/Service/NumberFormatterTest.php
@@ -7,31 +7,48 @@ use PHPUnit\Framework\TestCase;
 
 class NumberFormatterTest extends TestCase
 {
-  /**
-   * @return array
-   */
-  public function getData()
-  {
-    return [
-      ['2.84M', '2835779'],
-      ['535K', '535216'],
-      ['27 534', '27533.78'],
-      ['533.10', '533.1']
-    ];
-  }
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return [
+          ['1.00M', 999500],
+          ['-1.00M', -999500],
+          ['2.84M', 2835779],
+          ['-10.26M', -10255779],
 
-  /**
-   * @param $expected
-   * @param $number
-   * @dataProvider getData
-   */
-  public function testNumberFormatter($expected, $number)
-  {
-    $numberFormatter = new NumberFormatter();
-    $result = $numberFormatter->formatNumber($number);
-    $this->assertEquals($expected, $result);
+          ['100K', 99950],
+          ['124K', 123654.89],
+          ['-124K', -123654.89],
+          ['535K', 535216],
 
-  }
+          ['1 000', 999.9999],
+          ['-1 000', -999.9999],
+          ['8 024', 8023.91905],
+          ['-8 024', -8023.91905],
+          ['27 534', 27533.78],
 
+          ['533.10', 533.1],
+          ['66.67', 66.6666],
+          ['999.99', 999.99],
+          ['-999.99', -999.99],
+
+          ['12', 12.00],
+          ['-12', -12.00],
+        ];
+    }
+
+    /**
+     * @param $expected
+     * @param $number
+     * @dataProvider getData
+     */
+    public function testNumberFormatter($expected, $number)
+    {
+        $numberFormatter = new NumberFormatter();
+        $result = $numberFormatter->formatNumber($number);
+        $this->assertEquals($expected, $result);
+    }
 
 }
